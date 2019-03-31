f128694859756406d88618f4f192da40c901e1e0
Maarten Knapen <maarten.knapen@telenet.be>
2019-04-01 00:41:48 +0200

    Version 1

--- FindLargestPrimeFactor/Program.cs
+++ FindLargestPrimeFactor/Program.cs
@@ -10,6 +10,67 @@ namespace FindLargestPrimeFactor
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number to find it's largest prime factor:");
+            long input = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(FindLargestPrimeFactor(input));
+
+            Console.ReadKey();
+        }
+
+        public static long FindLargestPrimeFactor(long num) // finding the largest prime factor
+        {
+            long largestPrime = FindPrimeFactors(num).Max();
+
+            return largestPrime;
+        }
+
+        public static List<long> FindPrimeFactors(long num) // finding all the prime factors
+        {
+            List<long> primeFactors = new List<long>();
+            List<long> primeNumbers = FindPrimes(num);
+
+            bool CheckPrimeFactor(long x, long y) // function to check if number is prime factor
+            {
+                if (x % y == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            foreach (long n in primeNumbers)
+            {
+                while (CheckPrimeFactor(num, n))
+                {
+                    primeFactors.Add(n);
+                    num = num / n;
+                }
+            }
+
+            return primeFactors;
+        }
+
+        public static List<long> FindPrimes(long num) // Finding all prime numbers, based on the Sieve of Eratosthenes : https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+        {
+            List<long> primes = new List<long>();
+            List<long> noPrimes = new List<long>();
+
+            for (int i = 2; i < Math.Sqrt(num); i++)
+            {
+                for (int j = (int)Math.Pow(i, 2); j < num; j = j + i)
+                {
+                    noPrimes.Add(j);
+                }
+            }
+
+            for (int i = 2; i < num; i++)
+            {
+                if (!noPrimes.Contains(i))
+                {
+                    primes.Add(i);
+                }
+            }
+            return primes;
         }
     }
 }
